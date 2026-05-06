3963a888f85d4a7d937f4ad71250a41a841ab9bf
HlibPavlyk <pavlikgo78@gmail.com>
2026-05-06 22:26:24 +0300

    feat: add MediatR validation pipeline behavior and DI registration

---
+++ server/src/Coiny.Application/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,21 @@
+using Coiny.Application.Common.Behaviors;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Coiny.Application.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddMediatR(cfg =>
+            cfg.RegisterServicesFromAssembly(typeof(IApplicationMarker).Assembly));
+
+        services.AddValidatorsFromAssemblyContaining<IApplicationMarker>();
+
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+        return services;
+    }
+}
---
+++ server/src/Coiny.Application/IApplicationMarker.cs
@@ -0,0 +1,4 @@
+namespace Coiny.Application;
+
+/// <summary>Assembly marker for MediatR handler and FluentValidation validator discovery.</summary>
+public interface IApplicationMarker;
