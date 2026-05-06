9d9b77127b87e6cbd4c375cdd1c203494c476643
HlibPavlyk <pavlikgo78@gmail.com>
2026-05-06 22:29:21 +0300

    feat: add ResultTransformFilter, GlobalExceptionHandler, and wire controllers

--- server/src/Coiny.Api/Program.cs
+++ server/src/Coiny.Api/Program.cs
@@ -1,8 +1,17 @@
+using Coiny.Api.Filters;
+using Coiny.Api.Middleware;
 using Coiny.Infrastructure.Persistence.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDatabase(builder.Configuration);
 
+builder.Services.AddProblemDetails();
+builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+builder.Services.AddControllers(opts => opts.Filters.Add<ResultTransformFilter>());
+
 var app = builder.Build();
+
+app.UseExceptionHandler();
+app.MapControllers();
 app.Run();
